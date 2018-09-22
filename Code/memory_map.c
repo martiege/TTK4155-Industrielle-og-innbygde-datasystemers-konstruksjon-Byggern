9c464df7eb60c21b2d9f08b7498162f334fd1019
Unknown <martiege@stud.ntnu.no>
2018-09-22 12:50:40 +0200

    Added read ADC function to memory_map.h

--- Code/memory_map.c
+++ Code/memory_map.c
@@ -8,6 +8,13 @@ volatile char* get_mem()
 	return memory; 
 }
 
+uint8_t memory_read_ADC(CHANNEL c)
+{
+	memory[ADC] = c;
+	_delay_ms(1);
+	return (memory[ADC] & 0x00ff);
+}
+
 void memory_write_oled_command(uint8_t com)
 {
 	memory[OLED_command] = com;
@@ -17,3 +24,4 @@ void memory_write_oled_data(uint8_t com)
 {
 	memory[OLED_data] = com;
 }
+
--- Code/memory_map.h
+++ Code/memory_map.h
@@ -3,15 +3,25 @@
 
 #include <stdio.h>
 
-enum ADDR 
+typedef enum ADDR 
 {
 	OLED_command = 0x1000, 
 	OLED_data    = 0x1200, 
 	ADC          = 0x1400, 
 	SRAM         = 0x1800
-};
+} ADDR;
+
+typedef enum CHANNEL 
+{
+	CH1 = 0b0100, 
+	CH2 = 0b0101, 
+	CH3 = 0b0110, 
+	CH4 = 0b0111
+} CHANNEL;
 
 volatile char* get_mem();
+
+uint8_t memory_read_ADC(CHANNEL c)
 
 void memory_write_oled_command(uint8_t com);
 
