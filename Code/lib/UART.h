cb30dc48d6bb81daf054a0959bf9979e82c9c8d3
Unknown <martiege@stud.ntnu.no>
2018-11-20 10:33:06 +0100

    UART modularization

--- Code/lib/UART.h
+++ Code/lib/UART.h
@@ -1,21 +1,59 @@
 #ifndef UART_H
 #define UART_H
 
-
-
 #include <avr/io.h>
 #include <stdio.h>
 
 
-/* UART module for ATmega162 and ATmega 2560*/
+/************************************************************
+Universal Asynchronous serial Receiver and Transmitter (UART)
+
+Implements UART module for ATmega162 and ATmega 2560
+
+There is support for Universal Synchronous and Asynchronous
+serial Receiver and Transmitter, though this is not necessary
+for this system. 
+
+Uses 8 bit data, 2 stop bits, baudrate 9600 and normal 
+transmission speed. 
+
+The functions implemented are:
+
+	UART_Init(unsigned int ubrr)
+	
+		Initializes the module for the settings described.
+		The ubrr parameter is calculated based on the 
+		baudrate and the CPU frequency, see defines.h
+		
+	UART_Transmit(unsigned char data)	
+		
+		Transmitts the parameter once the transmit buffer
+		is empty. 
+		
+	UART_Receive()
+	
+		Waits for data to be recieved. Returns the value 
+		recieved. 
+		
+	put_char(char data, FILE* file)
+		
+		Wrapper function for UART_Transmit, to be used with
+		the printf function. Also implements transmitting
+		'\r' when '\n' is transmitted, for easier formatting. 
+		
+	get_char(FILE* file)
+	
+		Wrapper function for UART_Receive,to be used with
+		the printf function. Returns the return value 
+		from UART_Receive.
+
+************************************************************/
 
 void UART_Init(unsigned int ubrr);
 
 void UART_Transmit(unsigned char data);
 
 char UART_Receive();
-
-void UART_Send(unsigned char data);
 
 int put_char(char data, FILE* file);
 
